f92bb0b9ebdea52799cbbf0a49e83452c8abdc52
Nate <natejoe@gmail.com>
2016-05-03 17:23:07 -0400

    Set up the main GUI features, monster, food, and lives.

--- mylittlemonster/ViewController.swift
+++ mylittlemonster/ViewController.swift
@@ -10,9 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //IBOutlets
+    @IBOutlet weak var monsterImg: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        var imgArray = [UIImage]()
+        
+        //load in monster images
+        for x in 1..<5 {
+            let img = UIImage(named: "idle\(x).png")
+            imgArray.append(img!)
+        }
+        
+        //set the animation to the monster ImageView
+        monsterImg.animationImages = imgArray
+        
+        //set the animation duration for the monster
+        monsterImg.animationDuration = 0.8
+        
+        //when repeat count is 0 it will always repeat
+        monsterImg.animationRepeatCount = 0
+        
+        monsterImg.startAnimating()
     }
 
 
